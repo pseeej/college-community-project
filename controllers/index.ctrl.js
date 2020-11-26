57d5e3b8d566b12a6524993ca5dc9355aff3f080
박윤환 <pyh8618@gmail.com>
2020-11-26 16:25:30 +0900

    index.html css 변경 -pyh

--- controllers/index.ctrl.js
+++ controllers/index.ctrl.js
@@ -1,7 +1,13 @@
 const models = require('../models');
 
 exports.get_index = ( _ , res) => {
-    res.render('index.html');
+    //res.render('index.html');
+    models.posts.findAll({
+
+    }).then( (post) => {
+        // DB에서 받은 posts를 posts변수명으로 내보냄
+        res.render( 'index.html' ,{ post});
+    });
 }
 
 exports.get_index_free = ((req, res) => {
